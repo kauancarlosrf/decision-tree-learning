27379092c1cc129c86a2ab8608561416db91796a
kauancarlosrf <kauancarlosrf@gmail.com>
2024-11-30 17:31:37 -0300

    Fazendo alguns ajustes no exemplo do livro

--- dataSet.py
+++ dataSet.py
@@ -1,13 +1,13 @@
 # Exemplo de dados
 
 dataSetRestaurante = [
-    {'preço': 'caro', 'dia_da_semana': 'sexta', 'está_chovendo': 'sim', 'vai_esperar': 'não'},
-    {'preço': 'médio', 'dia_da_semana': 'sexta', 'está_chovendo': 'não', 'vai_esperar': 'não'},
+    {'preço': 'caro', 'dia_da_semana': 'sexta', 'está_chovendo': 'sim', 'vai_esperar': 'sim'},
+    {'preço': 'médio', 'dia_da_semana': 'sexta', 'está_chovendo': 'não', 'vai_esperar': 'sim'},
     {'preço': 'barato', 'dia_da_semana': 'sexta', 'está_chovendo': 'não', 'vai_esperar': 'sim'},
-    {'preço': 'caro', 'dia_da_semana': 'sábado', 'está_chovendo': 'não', 'vai_esperar': 'não'},
+    {'preço': 'caro', 'dia_da_semana': 'sábado', 'está_chovendo': 'não', 'vai_esperar': 'sim'},
     {'preço': 'barato', 'dia_da_semana': 'sábado', 'está_chovendo': 'sim', 'vai_esperar': 'sim'},
     {'preço': 'médio', 'dia_da_semana': 'sábado', 'está_chovendo': 'sim', 'vai_esperar': 'sim'},
-    {'preço': 'caro', 'dia_da_semana': 'segunda', 'está_chovendo': 'não', 'vai_esperar': 'não'},
+    {'preço': 'caro', 'dia_da_semana': 'segunda', 'está_chovendo': 'não', 'vai_esperar': 'sim'},
     {'preço': 'médio', 'dia_da_semana': 'segunda', 'está_chovendo': 'sim', 'vai_esperar': 'não'},
     {'preço': 'barato', 'dia_da_semana': 'segunda', 'está_chovendo': 'não', 'vai_esperar': 'sim'},
     {'preço': 'barato', 'dia_da_semana': 'sexta', 'está_chovendo': 'sim', 'vai_esperar': 'sim'}
@@ -26,40 +26,40 @@ for amostra in dataSetRestaurante:
 
 # Novo dataset para prever o resultado de um jogo de futebol com 50 casos
 dataSetFutebol = [
-    {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
-    {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
-    {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
-    {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
-    {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
-    {'temperatura': 'medio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'vencer'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
+    {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
+    {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'medio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
     {'temperatura': 'quente', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
     {'temperatura': 'medio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'vencer'},
-    {'temperatura': 'quente', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
     {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim', 'resultado': 'vencer'},
     {'temperatura': 'medio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
     {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
     {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
     {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
     {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
     {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
     {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
     {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'quente', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
     {'temperatura': 'medio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'vencer'},
     {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
-    {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
+    {'temperatura': 'frio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'talvez'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
     {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
-    {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
+    {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim', 'resultado': 'talvez'},
     {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
     {'temperatura': 'quente', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
     {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
@@ -67,7 +67,8 @@ dataSetFutebol = [
     {'temperatura': 'frio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'perder'},
     {'temperatura': 'quente', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
     {'temperatura': 'frio', 'hora_do_jogo': 'tarde', 'condicao_do_campo': 'ruim', 'resultado': 'perder'},
-    {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'vencer'}
+    {'temperatura': 'medio', 'hora_do_jogo': 'noite', 'condicao_do_campo': 'bom', 'resultado': 'vencer'},
+    {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim', 'resultado': 'vencer'}
 ]
 
 data_frame_dados_futebol = []
@@ -79,3 +80,39 @@ for amostra in dataSetFutebol:
     
     data_frame_dados_futebol.append(atributos)
     data_frame_decisoes_futebol.append(classe)
+
+
+dataSetRestaurantBook = [
+    {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Some", "Price": "$$$", "Rain": "No", "Res": "Yes", "Type": "French", "Est": "0-10", "WillWait": "Yes"},
+    {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Full", "Price": "$", "Rain": "No", "Res": "Yes", "Type": "Thai", "Est": "30-60", "WillWait": "No"},
+   # {"Alt": "No", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Some", "Price": "$", "Rain": "No", "Res": "No", "Type": "Burger", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "Yes", "Bar": "No", "Fri": "Yes", "Hun": "Yes", "Pat": "Full", "Price": "$", "Rain": "Yes", "Res": "Yes", "Type": "Thai", "Est": "10-30", "WillWait": "Yes"},
+   # {"Alt": "Yes", "Bar": "No", "Fri": "No", "Hun": "No", "Pat": "Full", "Price": "$$$", "Rain": "No", "Res": "Yes", "Type": "French", "Est": ">60", "WillWait": "No"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Some", "Price": "$$", "Rain": "Yes", "Res": "No", "Type": "Italian", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "No", "Bar": "No", "Fri": "No", "Hun": "No", "Pat": "None", "Price": "$", "Rain": "No", "Res": "No", "Type": "Burger", "Est": "0-10", "WillWait": "No"},
+   # {"Alt": "No", "Bar": "No", "Fri": "No", "Hun": "Yes", "Pat": "Some", "Price": "$$", "Rain": "Yes", "Res": "Yes", "Type": "Thai", "Est": ">60", "WillWait": "Yes"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "No", "Hun": "Yes", "Pat": "Full", "Price": "$$$", "Rain": "No", "Res": "Yes", "Type": "Burger", "Est": "10-30", "WillWait": "No"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "No", "Pat": "Full", "Price": "$$", "Rain": "Yes", "Res": "Yes", "Type": "Italian", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "No", "Bar": "Yes", "Fri": "No", "Hun": "Yes", "Pat": "None", "Price": "$", "Rain": "No", "Res": "No", "Type": "Thai", "Est": "30-60", "WillWait": "No"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Full", "Price": "$", "Rain": "No", "Res": "No", "Type": "Burger", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "No", "Pat": "Full", "Price": "$$", "Rain": "Yes", "Res": "Yes", "Type": "Italian", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "No", "Bar": "Yes", "Fri": "No", "Hun": "Yes", "Pat": "None", "Price": "$", "Rain": "No", "Res": "No", "Type": "Thai", "Est": "30-60", "WillWait": "No"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Full", "Price": "$", "Rain": "No", "Res": "No", "Type": "Burger", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "No", "Pat": "Full", "Price": "$$", "Rain": "Yes", "Res": "Yes", "Type": "Italian", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "No", "Bar": "Yes", "Fri": "No", "Hun": "Yes", "Pat": "None", "Price": "$", "Rain": "No", "Res": "No", "Type": "Thai", "Est": "30-60", "WillWait": "No"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Full", "Price": "$", "Rain": "No", "Res": "No", "Type": "Burger", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "No", "Pat": "Full", "Price": "$$", "Rain": "Yes", "Res": "Yes", "Type": "Italian", "Est": "0-10", "WillWait": "Yes"},
+   # {"Alt": "No", "Bar": "Yes", "Fri": "No", "Hun": "Yes", "Pat": "None", "Price": "$", "Rain": "No", "Res": "No", "Type": "Thai", "Est": "30-60", "WillWait": "No"},
+   # {"Alt": "Yes", "Bar": "Yes", "Fri": "Yes", "Hun": "Yes", "Pat": "Full", "Price": "$", "Rain": "No", "Res": "No", "Type": "Burger", "Est": "0-10", "WillWait": "Yes"}
+    
+]
+
+data_frame_dados_restaurant_book = []
+data_frame_decisoes_restaurant_book = []
+
+for amostra in dataSetRestaurantBook:
+    atributos = {k: v for k, v in amostra.items() if k != 'WillWait'}
+    classe = amostra['WillWait']
+    
+    data_frame_dados_restaurant_book.append(atributos)
+    data_frame_decisoes_restaurant_book.append(classe)
--- usandoLibArvore.py
+++ usandoLibArvore.py
@@ -1,5 +1,7 @@
 from arvore import construir_arvore, prever
-from dataSet import data_frame_dados_futebol, data_frame_decisoes_futebol, data_frame_dados_restaurante, data_frame_decisoes_restaurante
+from dataSet import data_frame_dados_futebol, data_frame_decisoes_futebol, data_frame_dados_restaurante, data_frame_decisoes_restaurante, data_frame_dados_restaurant_book, data_frame_decisoes_restaurant_book
+
+import graphviz
 
 
 # Construir a árvore treinada no DataSet RESTAURANTE
@@ -20,3 +22,34 @@ novo_exemplo21 = {'temperatura': 'quente', 'hora_do_jogo': 'noite', 'condicao_do
 novo_exemplo22 = {'temperatura': 'medio', 'hora_do_jogo': 'manha', 'condicao_do_campo': 'ruim'}
 print("Decisão 1 árvore-futebol:", prever(arvore_futebol, novo_exemplo21))  # Espera "perder" ou "ganhar" com base na árvore
 print("Decisão 2 árvore-futebol:", prever(arvore_futebol, novo_exemplo22))  # Espera "perder" ou "ganhar" com base na árvore
+
+# Construir a árvore treinada no DataSet RESTAURANT BOOK (exemplo do livro de referência)
+arvore_restaurant_book = construir_arvore(data_frame_dados_restaurant_book, data_frame_decisoes_restaurant_book)
+
+# Testando a árvore de decisão em um novo exemplo
+novo_exemplo31 = {'Alt': 'No', 'Bar': 'No', 'Fri': 'Yes', 'Hun': 'Yes', 'Pat': 'Some', 'Price': '$$$', 'Rain': 'No', 'Res': 'Yes', 'Type': 'French', 'Est': '0-10'}
+novo_exemplo32 = {'Alt': 'Yes', 'Bar': 'No', 'Fri': 'No', 'Hun': 'No', 'Pat': 'Full', 'Price': '$$$', 'Rain': 'Yes', 'Res': 'No', 'Type': 'Italian', 'Est': '>60'}
+print("Decisão 1 árvore-restaurant-book:", prever(arvore_restaurant_book, novo_exemplo31))  # Espera "Yes" ou "No" com base na árvore
+print("Decisão 2 árvore-restaurant-book:", prever(arvore_restaurant_book, novo_exemplo32))  # Espera "Yes" ou "No" com base na árvore
+
+
+
+
+def visualizar_arvore(no, nome='Arvore_Decisao'):
+    def construir_grafo(no, grafo, caminho=''):
+        if no.folha:
+            grafo.node(caminho, f'{no.classe}', shape='box', style='filled', color='lightgrey')
+        else:
+            grafo.node(caminho, f'{no.atributo}')
+            for filho in no.filhos:
+                novo_caminho = f'{caminho}->{filho.valor}'
+                grafo.edge(caminho, novo_caminho, label=f'{filho.valor}')
+                construir_grafo(filho, grafo, novo_caminho)
+    
+    grafo = graphviz.Digraph(nome, format='png')
+    construir_grafo(no, grafo)
+    grafo.render(view=False)  # Salva e não abre o arquivo gerado
+
+
+#visualizar_arvore(arvore_restaurant_book)
+visualizar_arvore(arvore_futebol)
